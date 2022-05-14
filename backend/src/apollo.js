50b9111a67f2d1039f92b51651dd469bd900fa26
lokk3d <info@alfonsograziano.it>
2022-05-14 08:33:07 +0200

    Schema file updated

--- backend/src/apollo.js
+++ backend/src/apollo.js
@@ -2,23 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 const { ApolloServer, gql } = require('apollo-server');
 
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Task" type defines the queryable fields for every task in our data source.
-  type Task {
-    status: String
-    name: String,
-    id: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each.
-  type Query {
-    tasks: [Task],
-    
-  }
-`;
+const typeDefs = require("./schema")
 
 const mockDataSet = [
   {
---
+++ backend/src/schema.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+
+    type Query{
+        "Query to get tracks for the homepage"
+        tracksForHome: [Track!]!
+    }
+
+  type Track{
+      id: ID!
+      title: String!
+      author: Author!
+      thumbnail: String
+      length:Int
+      modulesCount: Int
+  }
+
+  type Author{
+      id:ID!
+      name:String!
+      photo:String
+  }
+`;
+
+module.exports = typeDefs
